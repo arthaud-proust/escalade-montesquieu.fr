400f831312505c0c12ad042b9daba1b4c43f5c4a
Arthaud Proust <arthaudp33@gmail.com>
2021-03-01 14:47:31 +0100

    fix migration

--- database/migrations/2021_03_01_094532_users_add_email_preferences.php
+++ database/migrations/2021_03_01_094532_users_add_email_preferences.php
@@ -14,7 +14,7 @@ class UsersAddEmailPreferences extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('email_preferences')->nullable();
+            $table->string('email_preferences')->default(' ');
         });
     }
 
